a7fa9a30b774820f2591368d45182158ab515fb5
xiezg <xiezg@317hu.com>
2017-05-22 14:13:29 +0800

    insist-rpc查看平台

---
+++ src/main/java/com/bozhong/insist/App.java
@@ -0,0 +1,13 @@
+package com.bozhong.insist;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
